e1e1c763ef84b84b21b63b086dfd32cf2e01e755
alexiajackson <alexia.jackson.01@gmail.com>
2022-09-18 18:57:22 -0700

    used javascript for loops to make style changes to html content

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Select some elements...
+let myHeader = document.querySelector("#page-header") 
+myHeader.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll(".dog-image")
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = "50px"
+}
+
+let dogNames = document.querySelectorAll(".dog-name")
+for (let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.color = "pink"
+}
+
+let myBackground = document.querySelector("body")
+myBackground.style.backgroundColor = "yellow"
